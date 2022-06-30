b8ff30db4cf562ae28079099fdae23bed25789c4
wkndavid <workingnown@gmail.com>
2022-06-30 13:17:04 -0300

    Exercícios de Condicionais

--- site/script.js
+++ site/script.js
@@ -1,1 +1,21 @@
-alert('olaaaá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    // var data = new Date()
+    // var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia
+        img.src = './image/manhã.png'
+        document.body.style.background = '#578A6B'
+    } else if(hora >= 12 && hora < 18) {
+        // Boa tarde
+        img.src = './image/tarde.png'
+        document.body.style.background = '#F1A65F'
+    } else {
+        // Boa noite
+        img.src = './image/noite.png'
+        document.body.style.background = '#0E0D1F'
+    }
+}
